9b22082ade851676bbb775fc2b79b9cc9b8b481f
jareershafiq125 <jareershafiq22@gmail.com>
2024-12-10 00:14:54 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+
+import Coursel from '@/components/Coursel'
+import Footer from '@/components/Footer'
+import Header from '@/components/header'
+import Navbar from '@/components/navbar'
+import Rating from '@/components/Rating'
+import Slider from '@/components/Sliders'
+import React from 'react'
+
+function page() {
+  return (
+    
+<><><>
+<Header/>
+<Navbar />
+
+      <Coursel />
+      <Slider /></>
+      <Rating /></>
+     <Footer/>
+      </>
+
+  )
+}
+
+export default page
